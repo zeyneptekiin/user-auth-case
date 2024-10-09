84db8fcd77ebdce38b35222085dcb6844094b6a6
zeyneptekin <zeyneptekin1682@gmail.com>
2024-10-09 22:06:25 +0300

    [Update] CORS settings in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://199.247.17.44:3001/:path*',
+            },
+        ];
+    },
+};
 
 export default nextConfig;
