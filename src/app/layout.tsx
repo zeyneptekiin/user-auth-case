4c99f96e4b04341eed1b33514e9ef19c22daaa02
zeyneptekin <zeyneptekin1682@gmail.com>
2024-10-09 14:30:20 +0300

    [Add] Navbar Component to Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 
 
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body
         className={`antialiased`}
       >
+       <Navbar />
         {children}
       </body>
     </html>
