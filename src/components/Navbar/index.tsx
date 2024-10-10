039aa4591909d14a976bfd04b3669d2513a81f5d
zeyneptekin <zeyneptekin1682@gmail.com>
2024-10-10 22:33:36 +0300

    [Add] color change on click for Navbar links

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,15 +1,43 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+    const pathname = usePathname();
+
     return (
         <nav className="bg-primary-blue p-4 text-white">
             <div className="max-w-7xl mx-auto flex justify-between">
-                <Link href="/" className="hover:underline font-semibold">Home</Link>
+                <Link
+                    href="/"
+                    className={`hover:underline font-semibold ${
+                        pathname === "/" ? "text-black-blue" : "text-white"
+                    }`}
+                >
+                    Home
+                </Link>
+
                 <div className="flex gap-4">
-                    <Link href="/signup" className="hover:underline font-semibold">Sign Up</Link>
-                    <Link href="/login" className="hover:underline font-semibold">Log In</Link>
+                    <Link
+                        href="/signup"
+                        className={`hover:underline font-semibold ${
+                            pathname === "/signup" ? "text-black-blue" : "text-white"
+                        }`}
+                    >
+                        Sign Up
+                    </Link>
+
+                    <Link
+                        href="/login"
+                        className={`hover:underline font-semibold ${
+                            pathname === "/login" ? "text-black-blue" : "text-white"
+                        }`}
+                    >
+                        Log In
+                    </Link>
                 </div>
             </div>
         </nav>
     );
-};
+}
