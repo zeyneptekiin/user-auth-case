69dca67224cc3ea012b616dbd5f234fd5a4ec77d
zeyneptekin <zeyneptekin1682@gmail.com>
2024-10-12 23:17:51 +0300

    [Fix] wrap all OtpForm function in Suspense on OTP page

--- src/app/login/verify/page.tsx
+++ src/app/login/verify/page.tsx
@@ -10,7 +10,7 @@ type OtpFormInputs = {
     otp: string;
 };
 
-export default function Verify() {
+function OtpForm() {
     const router = useRouter();
     const searchParams = useSearchParams();
     const email = searchParams.get('email');
@@ -38,32 +38,38 @@ export default function Verify() {
     };
 
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <div className="flex items-center justify-center min-h-screen p-8 bg-lightest-blue">
-                <div className="w-[400px] text-center bg-lighter-blue px-6 py-8 rounded-2xl shadow-xl">
-                    <h3 className="text-lg font-semibold mb-1">OTP Verification</h3>
+        <div className="flex items-center justify-center min-h-screen p-8 bg-lightest-blue">
+            <div className="w-[400px] text-center bg-lighter-blue px-6 py-8 rounded-2xl shadow-xl">
+                <h3 className="text-lg font-semibold mb-1">OTP Verification</h3>
 
-                    <p className="text-center text-sm text-darker-blue mb-4">
-                        The OTP code has been sent to {email}.
-                    </p>
+                <p className="text-center text-sm text-darker-blue mb-4">
+                    The OTP code has been sent to {email}.
+                </p>
 
-                    <form onSubmit={handleSubmit(onOtpSubmit)}>
-                        <OtpInput
-                            value={otp}
-                            onChange={setOtp}
-                            numInputs={6}
-                            renderInput={(props) => <input {...props} />}
-                            containerStyle="mb-4"
-                            inputStyle={`mx-auto !w-10 h-10 border rounded ${errors.otp ? 'border-red-500' : 'border-gray-300'}`}
-                        />
-                        {errorMessage && <p className="text-left text-red-500 text-sm mb-3 pl-2">{errorMessage}!</p>}
+                <form onSubmit={handleSubmit(onOtpSubmit)}>
+                    <OtpInput
+                        value={otp}
+                        onChange={setOtp}
+                        numInputs={6}
+                        renderInput={(props) => <input {...props} />}
+                        containerStyle="mb-4"
+                        inputStyle={`mx-auto !w-10 h-10 border rounded ${errors.otp ? 'border-red-500' : 'border-gray-300'}`}
+                    />
+                    {errorMessage && <p className="text-left text-red-500 text-sm mb-3 pl-2">{errorMessage}!</p>}
 
-                        <button type="submit" className="w-full bg-primary-blue text-white px-4 py-2 rounded hover:bg-midnight-blue transition">
-                            Submit
-                        </button>
-                    </form>
-                </div>
+                    <button type="submit" className="w-full bg-primary-blue text-white px-4 py-2 rounded hover:bg-midnight-blue transition">
+                        Submit
+                    </button>
+                </form>
             </div>
+        </div>
+    );
+}
+
+export default function Verify() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <OtpForm />
         </Suspense>
     );
 }
