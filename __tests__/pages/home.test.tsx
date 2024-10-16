b430ad944f10f4b1b930db1fed443e1d91f3c05f
zeyneptekin <zeyneptekin1682@gmail.com>
2024-10-17 00:50:07 +0300

    [Add] mockUseAuthStore for Home Page

--- __tests__/pages/home.test.tsx
+++ __tests__/pages/home.test.tsx
@@ -1,9 +1,22 @@
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import Home from '@/app/page';
-import '@testing-library/jest-dom';
+import { getCookie } from 'cookies-next';
+import { useAuthStore } from '../../src/store/authStore';
+
+jest.mock('cookies-next', () => ({
+    getCookie: jest.fn(),
+}));
+
+jest.mock('../../src/store/authStore', () => ({
+    useAuthStore: jest.fn(),
+}));
 
 describe('Home Page', () => {
-    it('renders welcome message and description', () => {
+    it('renders welcome message and description when not logged in', () => {
+        (getCookie as jest.Mock).mockReturnValue(null);
+        (useAuthStore as unknown as jest.Mock).mockReturnValue({ userName: '' });
+
         render(<Home />);
 
         const welcomeMessage = screen.getByText(/welcome!/i);
@@ -14,6 +27,9 @@ describe('Home Page', () => {
     });
 
     it('renders sign up and log in links', () => {
+        (getCookie as jest.Mock).mockReturnValue(null);
+        (useAuthStore as unknown as jest.Mock).mockReturnValue({ userName: '' });
+
         render(<Home />);
 
         const signUpLink = screen.getByRole('link', { name: /sign up/i });
@@ -23,5 +39,18 @@ describe('Home Page', () => {
         const logInLink = screen.getByRole('link', { name: /log in/i });
         expect(logInLink).toBeInTheDocument();
         expect(logInLink).toHaveAttribute('href', '/login');
+    });
+
+    it('renders user information when logged in', () => {
+        (getCookie as jest.Mock).mockReturnValue('authToken');
+        (useAuthStore as unknown as jest.Mock).mockReturnValue({ userName: 'John Doe' });
+
+        render(<Home />);
+
+        const welcomeMessage = screen.getByText(/welcome john doe!/i);
+        expect(welcomeMessage).toBeInTheDocument();
+
+        const loginSuccessMessage = screen.getByText(/your login has been successfully completed/i);
+        expect(loginSuccessMessage).toBeInTheDocument();
     });
 });
